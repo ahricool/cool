5c095fe659ea0703fceb893f45351083a5811aa0
Ahri <sexyahri@qq.com>
2026-05-14 15:13:25 +0800

    Add Fastify hello world server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async () => {
+  return { message: 'Hello, World!' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
